e33f25985ab0f49b7e0ecda786a0ff7174b3dd14
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-09-22 22:33:14 +0300

    Setup home screen UI

--- app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/screens/home/HomeScreen.kt
@@ -1,12 +1,19 @@
 package com.apolloagriculture.android.takehomeassignment.ui.screens.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.apolloagriculture.android.takehomeassignment.ui.components.ForecastCard
 import com.apolloagriculture.android.takehomeassignment.ui.theme.ApolloAndroidTheme
 import org.koin.androidx.compose.getViewModel
 
@@ -18,7 +25,24 @@ fun HomeScreen(
 
     val forecast = viewModel.forecast.collectAsState().value
 
-    Text(text = "Forecast: \n $forecast")
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+
+        forecast?.let {
+            items(items = forecast) {
+                ForecastCard(
+                    modifier = Modifier.fillMaxWidth(),
+                    forecast = it
+                )
+            }
+        }
+
+    }
 
 }
 
