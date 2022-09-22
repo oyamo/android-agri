68527c52c8fe2027c261d91338204f87da2d9493
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-09-22 23:15:47 +0300

    Updated vm to use UiState

--- app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/screens/home/HomeViewModel.kt
@@ -1,28 +1,28 @@
 package com.apolloagriculture.android.takehomeassignment.ui.screens.home
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.apolloagriculture.android.takehomeassignment.domain.models.Forecast
 import com.apolloagriculture.android.takehomeassignment.domain.repository.ForecastRepository
+import com.apolloagriculture.android.takehomeassignment.utils.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class HomeViewModel constructor(private val forecastRepository: ForecastRepository) : ViewModel() {
 
-    private val _forecast = MutableStateFlow<List<Forecast>?>(null)
-    val forecast = _forecast.asStateFlow()
+    private val _forecastUiState = MutableStateFlow<UiState<List<Forecast>>>(UiState.Loading())
+    val forecastUiState = _forecastUiState.asStateFlow()
 
     init {
         fetchForecast()
     }
 
     private fun fetchForecast() = viewModelScope.launch {
-        forecastRepository.getForecast().collect { foreCasts ->
-            Log.e("TAG", "Response in VM: $foreCasts")
+        _forecastUiState.value = UiState.Loading()
 
-            _forecast.value = foreCasts
+        forecastRepository.getForecast().collect { foreCasts ->
+            _forecastUiState.value = UiState.Success(data = foreCasts)
         }
     }
 
