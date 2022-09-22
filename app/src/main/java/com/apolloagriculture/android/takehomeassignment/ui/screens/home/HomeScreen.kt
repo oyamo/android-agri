04a9ab188c2777870341a83c6e71053e0c7105b2
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-09-22 19:22:51 +0300

    Setup home screen

---
+++ app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,30 @@
+package com.apolloagriculture.android.takehomeassignment.ui.screens.home
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.apolloagriculture.android.takehomeassignment.ui.theme.ApolloAndroidTheme
+import org.koin.androidx.compose.getViewModel
+
+@Composable
+fun HomeScreen(
+    paddingValues: PaddingValues = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
+    viewModel: HomeViewModel = getViewModel()
+) {
+
+    val forecast = viewModel.forecast.observeAsState().value
+
+    Text(text = "Forecast: \n $forecast")
+
+}
+
+@Preview()
+@Composable
+fun Preview() {
+    ApolloAndroidTheme {
+        HomeScreen()
+    }
+}
