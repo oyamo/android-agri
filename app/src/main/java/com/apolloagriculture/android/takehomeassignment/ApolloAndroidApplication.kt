089625134d603dab67fff9e4b7dcb7e2473fb199
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-09-22 17:52:24 +0300

    Setup presentation module

---
+++ app/src/main/java/com/apolloagriculture/android/takehomeassignment/ApolloAndroidApplication.kt
@@ -0,0 +1,29 @@
+package com.apolloagriculture.android.takehomeassignment
+
+import android.app.Application
+import com.apolloagriculture.android.takehomeassignment.di.presentationModule
+import com.apolloagriculture.android.takehomeassignment.network.di.networkModule
+import com.apolloagriculture.android.takehomeassignment.repository.di.repositoryModule
+import org.koin.android.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+import timber.log.Timber
+
+class ApolloAndroidApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val modules = listOf(networkModule, repositoryModule, presentationModule)
+        startKoin {
+            androidLogger(level = Level.NONE)
+            androidContext(this@ApolloAndroidApplication)
+            modules(modules)
+        }
+
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+}
---
+++ app/src/main/java/com/apolloagriculture/android/takehomeassignment/di/PresentationModule.kt
@@ -0,0 +1,5 @@
+package com.apolloagriculture.android.takehomeassignment.di
+
+import org.koin.dsl.module
+
+val presentationModule= module {  }
