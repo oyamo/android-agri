e36207e40279007b4e616cad3b5bd0621537b06a
Victor Kabata <39780120+VictorKabata@users.noreply.github.com>
2022-09-22 23:16:11 +0300

    Added app bar and removed padding values from composable params

--- app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/activity/MainActivity.kt
+++ app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/activity/MainActivity.kt
@@ -9,11 +9,18 @@ import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
+import com.apolloagriculture.android.takehomeassignment.R
 import com.apolloagriculture.android.takehomeassignment.ui.navigation.Navigation
 import com.apolloagriculture.android.takehomeassignment.ui.theme.ApolloAndroidTheme
 
@@ -44,12 +51,26 @@ fun MainScreen() {
 
     Scaffold(
         modifier = Modifier,
-        topBar = {},
-        bottomBar = {},
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.app_name),
+                        style = MaterialTheme.typography.h4,
+                        maxLines = 1,
+                        fontSize = 20.sp,
+                        overflow = TextOverflow.Ellipsis,
+                        textAlign = TextAlign.Center,
+                        color = MaterialTheme.colors.onSurface
+                    )
+                },
+                backgroundColor = MaterialTheme.colors.primary
+            )
+        },
         backgroundColor = MaterialTheme.colors.surface
-    ) { paddingValues ->
+    ) {
 
-        Navigation(navController = navController, paddingValues = paddingValues)
+        Navigation(navController = navController)
 
     }
 }
--- app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/navigation/Navigation.kt
+++ app/src/main/java/com/apolloagriculture/android/takehomeassignment/ui/navigation/Navigation.kt
@@ -1,6 +1,5 @@
 package com.apolloagriculture.android.takehomeassignment.ui.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
@@ -11,15 +10,12 @@ import com.apolloagriculture.android.takehomeassignment.ui.screens.home.HomeScre
 
 @ExperimentalMaterialApi
 @Composable
-fun Navigation(
-    navController: NavHostController,
-    paddingValues: PaddingValues,
-) {
+fun Navigation(navController: NavHostController) {
 
     NavHost(navController = navController, startDestination = NavigationItem.Home.route) {
 
         composable(route = NavigationItem.Home.route) {
-            HomeScreen(paddingValues)
+            HomeScreen()
         }
 
     }
